c079e2d5d1812d26d755c4179634b7a9f84e11b0
Celine Chan <cctinlone@gmail.com>
2023-03-05 17:06:38 +0100

    lil update on using Select

--- src/Components/RoomTypePerNight.js
+++ src/Components/RoomTypePerNight.js
@@ -2,8 +2,11 @@ import { useState } from "react";
 
 export default function RoomTypePerNight() {
   const [roomType, setRoomType] = useState("");
+  const [selectedFruit, setSelectedFruit] = useState("");
+
   const [night, setNight] = useState("1");
   const addNightNummer = Number(night);
+
   return (
     <>
       <h1>Our Price List for our 3 room types per night are as follow</h1>
@@ -12,8 +15,13 @@ export default function RoomTypePerNight() {
       <p>Superior Room - €220</p>
       <label>
         Room Type -
-        <input value={roomType} onChange={(e) => setRoomType(e.target.value)} />
+        <select value={roomType} onChange={(e) => setRoomType(e.target.value)}>
+          <option value="standard">Standard</option>
+          <option value="deluxe">Deluxe</option>
+          <option value="superior">Superior</option>
+        </select>
       </label>
+
       <label>
         Number of nights:
         <input
@@ -29,8 +37,27 @@ export default function RoomTypePerNight() {
           Add 1 night
         </button>
       </label>
-      {roomType !== "" && <p>Chosen Room Type: {roomType}. </p>}
+      <label>
+        Pick a fruit:
+        <select
+          value={selectedFruit}
+          onChange={(e) => setSelectedFruit(e.target.value)}
+        >
+          <option value="apple">Apple</option>
+          <option value="banana">Banana</option>
+          <option value="orange">Orange</option>
+        </select>
+      </label>
+
+      <p>Your chosen Room Type: {roomType} </p>
+      <p></p>
+      {/* {roomType !== "standard" && (
+        <p>Cost to pay for the chosen room :({night * 1}*90)</p>
+      )} */}
+
       {addNightNummer > 1 && <p>Number of night staying is: {night}.</p>}
+      <p></p>
+      <p>Your favorite fruit: {selectedFruit}</p>
     </>
   );
 }
